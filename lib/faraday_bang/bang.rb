a3c5ffd13229c9104fd7f1b1821afa725c53120d
Nicolás Hock Isaza <nhocki@gmail.com>
2015-01-29 13:21:48 -0500

    Allow blocks on Faraday connections

--- lib/faraday_bang/bang.rb
+++ lib/faraday_bang/bang.rb
@@ -2,8 +2,8 @@ module Faraday::Bang
   ERROR_CODES = [(400..417).to_a, (500..505).to_a].flatten
 
   Faraday::Connection::METHODS.each do |verb|
-    define_method("#{verb}!") do |*args|
-      response = self.send(verb, *args)
+    define_method("#{verb}!") do |*args, &block|
+      response = self.send(verb, *args, &block)
       if response.status >= 400
         err_name = "Response#{response.status}Error"
         if Faraday::Bang.const_defined?(err_name)
