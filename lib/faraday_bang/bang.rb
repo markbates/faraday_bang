1980bd0d3b6b9d201cdebb46a0346992c1c1d369
Nicolás Hock Isaza <nhocki@gmail.com>
2015-02-04 23:29:32 -0500

    Add support for `options!` method.
    Since Faraday doesn't support the `options` request as a first class
    HTTP method (see lostisland/faraday#305) we need to use the `run_request`
    method instead of the `options` one.

--- lib/faraday_bang/bang.rb
+++ lib/faraday_bang/bang.rb
@@ -1,22 +1,33 @@
 module Faraday::Bang
   ERROR_CODES = [(400..417).to_a, (500..505).to_a].flatten
+  SUPPORTED_HTTP_METHODS = Faraday::Connection::METHODS - Set.new([ :options ])
 
-  Faraday::Connection::METHODS.each do |verb|
+  SUPPORTED_HTTP_METHODS.each do |verb|
     define_method("#{verb}!") do |*args, &block|
       response = self.send(verb, *args, &block)
-      if response.status >= 400
-        err_name = "Response#{response.status}Error"
-        if Faraday::Bang.const_defined?(err_name)
-          klass = Faraday::Bang.const_get(err_name)
-          raise klass.new(response)
-        else
-          raise Faraday::Bang::ResponseError.new(response)
-        end
-      end
-      return response
+      handle_error(response)
     end
   end
 
+  define_method("options!") do |*args, &block|
+    url, params, headers = args
+    response = run_request(:options, url, params, headers, &block)
+    handle_error(response)
+  end
+
+  private
+  def handle_error(response)
+    if response.status >= 400
+      err_name = "Response#{response.status}Error"
+      if Faraday::Bang.const_defined?(err_name)
+        klass = Faraday::Bang.const_get(err_name)
+        raise klass.new(response)
+      else
+        raise Faraday::Bang::ResponseError.new(response)
+      end
+    end
+    response
+  end
 end
 
 Faraday.extend(Faraday::Bang)
