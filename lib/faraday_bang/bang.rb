ed73b9e814fb22293d49456cee950b53c5a8d808
Nicolás Hock Isaza <nhocki@gmail.com>
2015-02-05 10:55:35 -0500

    Better names and no more metaprogramming on `options!`.
    * Rename `handle_error` for `handle_response`
    * Remove `define_method` for the `options!` method.

--- lib/faraday_bang/bang.rb
+++ lib/faraday_bang/bang.rb
@@ -1,22 +1,22 @@
 module Faraday::Bang
   ERROR_CODES = [(400..417).to_a, (500..505).to_a].flatten
-  SUPPORTED_HTTP_METHODS = Faraday::Connection::METHODS - Set.new([ :options ])
+  SUPPORTED_HTTP_METHODS = Faraday::Connection::METHODS - [ :options ]
 
   SUPPORTED_HTTP_METHODS.each do |verb|
     define_method("#{verb}!") do |*args, &block|
       response = self.send(verb, *args, &block)
-      handle_error(response)
+      handle_response(response)
     end
   end
 
-  define_method("options!") do |*args, &block|
-    url, params, headers = args
-    response = run_request(:options, url, params, headers, &block)
-    handle_error(response)
+  def options!(*args, &block)
+    url, body, headers = args
+    response = run_request(:options, url, body, headers, &block)
+    handle_response(response)
   end
 
   private
-  def handle_error(response)
+  def handle_response(response)
     if response.status >= 400
       err_name = "Response#{response.status}Error"
       if Faraday::Bang.const_defined?(err_name)
