efdc498401395da9133c70e86b22af3e42582558
Nicolás Hock Isaza <nhocki@gmail.com>
2015-02-05 10:56:59 -0500

    Adding tests for `options!`

--- test/lib/faraday_bang/bang_test.rb
+++ test/lib/faraday_bang/bang_test.rb
@@ -7,6 +7,7 @@ describe Faraday::Bang do
   let(:body) { {foo: 'bar'}.to_json }
 
   [Faraday, Faraday.new].each do |klass|
+    let(:response) { mock(body: body, status: 200) }
 
     Faraday::Bang::SUPPORTED_HTTP_METHODS.each do |verb|
 
@@ -14,12 +15,11 @@ describe Faraday::Bang do
         block = Proc.new do |req|
           raise "dummy error #{verb}"
         end
-        ->{ klass.send("#{verb}!", &block) }.must_raise RuntimeError
+        error = ->{ klass.send("#{verb}!", &block) }.must_raise RuntimeError
+        error.message.must_equal "dummy error #{verb}"
       end
 
       describe "##{verb}!" do
-
-        let(:response) { mock(body: body, status: 200) }
 
         before do
           klass.expects(verb).with(url, args).returns(response)
@@ -61,6 +61,53 @@ describe Faraday::Bang do
 
       end
 
+    end
+
+    describe "options!" do
+      it "passes a block" do
+        block = Proc.new do |req|
+          raise "dummy error options"
+        end
+        error = ->{ klass.send("options!", &block) }.must_raise RuntimeError
+        error.message.must_equal "dummy error options"
+      end
+
+      describe 'successful response' do
+        before do
+          klass.expects(:run_request).with(:options, url, args, nil).returns(response)
+        end
+
+        it 'passes the call onto Faraday' do
+          res = klass.send("options!", url, args)
+          res.body.must_equal body
+        end
+      end
+
+      describe 'unsuccessful response' do
+        let(:env) { OpenStruct.new(url: 'http://example.com') }
+        let(:response) { OpenStruct.new(body: body, status: 506, env: env) }
+
+        before do
+          klass.expects(:run_request).with(:options, url, args, nil).returns(response)
+        end
+
+        it 'raises an error if its a non-200 response without an explicit exception' do
+          ->{klass.send("options!", url, args)}.must_raise Faraday::Bang::ResponseError, body
+        end
+
+        Faraday::Bang::ERROR_CODES.each do |code|
+
+          context code do
+
+            let(:response) { OpenStruct.new(body: body, status: code, env: env) }
+
+            it "raises a Faraday::Bang::Response#{code}Error" do
+              ->{klass.send("options!", url, args)}.must_raise Faraday::Bang.const_get("Response#{code}Error")
+            end
+
+          end
+        end
+      end
     end
 
   end
